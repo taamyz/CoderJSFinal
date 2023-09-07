79f251176031a047344b4b31603aff2c8466ece5
taamyz <tamires.trindade15@gmail.com>
2023-09-06 22:03:24 -0300

    final project

--- contentful.js
+++ contentful.js
@@ -1,4 +1,60 @@
+const spaceId = "hpth0uulbqvw"
+const environmentId = "master"
+const accessToken = "mdsIAkZQUgeLOqVKG5Tk2UHvjpCa-rrj3crEMZzqQIM"
 
+const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
+
+console.log(url)
+
+const sectionTag = document.querySelector("section.grid")
+
+const grabData = function () {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const assets = data.includes.Asset
+
+            return data.items.map(item => {
+                const imageId = item.fields.image.sys.id
+                const imageData = assets.find(asset => asset.sys.id === imageId)
+
+                let imageUrl = ""
+                if (imageData) {
+                    imageUrl = imageData.fields.file.url
+                }
+
+                if (imageUrl.startsWith('//')) {
+                    imageUrl = 'https:' + imageUrl
+                }
+
+                console.log(imageUrl)
+
+                return {
+                    ...item.fields,
+                    image: imageUrl
+                }
+            })
+        })
+}
+
+grabData().then(data => {
+    console.log(data)
+
+    const content = data.map(item => `
+        <div class="item">
+            <img src="${item.image}">
+            <div class="title">
+                <h2>${item.coffeeName}</h2>
+                <p>${item.mainProperty}</p>
+            </div>
+            <p class="description">${item.propertyList2}</p>
+        </div>
+    `).join('');
+
+    sectionTag.innerHTML = content;
+})
+
+/*
 const spaceId = "hpth0uulbqvw"
 const environmentId = "master"
 const accessToken = "mdsIAkZQUgeLOqVKG5Tk2UHvjpCa-rrj3crEMZzqQIM"
@@ -6,6 +62,7 @@ const accessToken = "mdsIAkZQUgeLOqVKG5Tk2UHvjpCa-rrj3crEMZzqQIM"
 
 const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
 
+console.log(url)
 
 const sectionTag = document.querySelector("section.grid")
 
@@ -14,25 +71,33 @@ const grabData = function () {
    return fetch (url)
 
     .then(response => response.json())
-    .then(data =>{
+    .then(data => {
 
         const assets = data.includes.Asset
 
-        return data.items.map(item => {
-            let imageUrl = "image1.jpg"
+        return data.items.map(items => {
 
-            const imageId = item.fields.image.sys.id
+            let imageUrl = ${item.image}
+
+            const imageId = items.fields.image.sys.id
 
             const imageData = assets.find(asset => {
-                return asset.sys.id = imageId
+                asset.sys.id == imageId
             })
 
             if (imageData) {
                 imageUrl = imageData.fields.file.url
             }
+           
+            items.fields.image = imageUrl
 
-            item.fields.image = imageUrl
-            return item.fields
+                if (imageUrl.startsWith('//')) {
+                        imageUrl = imageUrl.split('//').join('https://')
+                        }
+
+            console.log(imageUrl)
+
+            return items.fields
         })
     })
 }
@@ -40,19 +105,19 @@ const grabData = function () {
 grabData().then(data => {
 
     console.log(data)
-
     sectionTag.innerHTML = ""
 
     data.forEach(item => {
         sectionTag.innerHTML = sectionTag.innerHTML + `
         <div class="item">
-            <img src =  ${item.coffeeImage}>
+                <img src = "${item.image}">
             <div class = "title">
                 <h2>${item.coffeeName}</h2>
                 <p>${item.mainProperty}</p>
             </div>
-            <p class="description">${item.propertyList}</p>
+            <p class="description">${item.propertyList2}</p>
         </div>
         `
     })
 })
+*/
